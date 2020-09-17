29038c0232be1f2e3f774e1d6d5ea904b99bdf12
Melanie Goncalves <melaniegoncalves5@gmail.com>
2020-09-17 15:58:23 -0700

    Context

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import './App.css';
-import ClickCounterTwo from './components/ClickCounterTwo';
-import Counter2 from './components/Counter2';
-import HoverCounterTwo from './components/HoverCounterTwo';
-import User from './components/User';
-
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
-      {/* <ClickCounterTwo />
-      <HoverCounterTwo />
-      <User render={(isLoggedIn) => isLoggedIn ? 'Melanie' : 'Guest'} /> */}
-      <Counter2 render={(count, incrementCount) => (
-        <ClickCounterTwo
-          count={count}
-          incrementCount={incrementCount} />
-      )} />
-      <Counter2
-        render={(count, incrementCount) => (
-          <HoverCounterTwo
-            count={count}
-            incrementCount={incrementCount} />
-        )} />
+      <UserProvider value="Melanie">
+        <ComponentC />
+      </UserProvider>
+
     </div>
   );
 }
