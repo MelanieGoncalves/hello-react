1c9b5878c844a706dfb99a25d244c47259565179
Melanie Goncalves <melaniegoncalves5@gmail.com>
2020-09-17 14:13:55 -0700

    Higher Order Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
-import ErrorBoundary from './components/ErrorBoundary';
-import Hero from './components/Hero';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 
 function App() {
   return (
     <div className="App">
-      <ErrorBoundary>
-        <Hero heroName="Batman"></Hero>
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Superman"></Hero>
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Joker"></Hero>
-      </ErrorBoundary>
-
+      <ClickCounter />
+      <HoverCounter />
     </div>
   );
 }
